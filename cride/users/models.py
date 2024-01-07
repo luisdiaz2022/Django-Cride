70cb731132ae3513fc78fd921239a877aaec34ce
Luis <luisedgucv@gmail.com>
2024-01-07 12:41:12 -0500

    Added user mpdel into database

--- cride/users/models.py
+++ cride/users/models.py
@@ -3,11 +3,12 @@
 # Django
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Utilities
 from cride.utils.models import CRideModel
 
-class User(cride, AbstractUser):
+class User(CRideModel, AbstractUser):
     """User model.
     
     Extend from Django's Abstract user, change the username field
@@ -17,12 +18,16 @@ class User(cride, AbstractUser):
     email = models.EmailField(
         'email address',
         unique=True,
-        error_message={
+        error_messages={
             'unique': 'A user with that email already exists.'
         }
     )
 
-    phone_number = models.CharField(max_length=17, blank=True)
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: +999999999. Up to 15 digits allowed."
+    )
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
@@ -41,3 +46,11 @@ class User(cride, AbstractUser):
         default=False,
         help_text='Set to true when the user have verified its email address.'
     )
+
+    def __str__(self):
+        """Return username."""
+        return self.username
+    
+    def get_short_name(self):
+        """Return username."""
+        return self.username
