26a1e20a23bcd3bbf218e69c80cfaf6a3031eba9
Luis <luisedgucv@gmail.com>
2024-01-14 20:33:55 -0500

    added module to send verifications email, added template for the email

--- cride/users/serializers/users.py
+++ cride/users/serializers/users.py
@@ -3,6 +3,8 @@
 # Django
 from django.contrib.auth import password_validation, authenticate
 from django.core.validators import RegexValidator
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 # Django REST Framework
 from rest_framework import serializers
@@ -71,10 +73,27 @@ class UserSignUpSerializer(serializers.Serializer):
     def create(self, data):
         """Handle user and profle creation."""
         data.pop('password_confirmation')
-        user = User.objects.create_user(**data)
-        profile = Profile.objects.create(user=user)
+        user = User.objects.create_user(**data, is_verified=False)
+        Profile.objects.create(user=user)
+        self.send_confirmation_email(user)
         return user
+    
+    def send_confirmation_email(self, user):
+        """Send account verification link to given user."""
+        verification_token = self.gen_verification_token(user)
+        subject = 'Welcome @{}! Verify your account to start using Comparte Ride'.format(user.username)
+        from_email = 'Comparte Ride <noreply@comparteride.com>'
+        content = render_to_string(
+            'emails/users/account_verification.html',
+            {'token': verification_token, 'user': user}
+            )
+        msg = EmailMultiAlternatives(subject, content, from_email, [user.email])
+        msg.attach_alternative(content, "text/html")
+        msg.send()
 
+    def gen_verification_token(self, user):
+        """Create JWT token that the user can use to verify its account."""
+        return 'abc'
 
 
 class UserLoginSerializer(serializers.Serializer):
@@ -91,6 +110,8 @@ class UserLoginSerializer(serializers.Serializer):
         user = authenticate(username=data['email'], password=data['password'])
         if not user:
             raise serializers.ValidationError('Invalid credentials')
+        if not user.is_verified:
+            raise serializers.ValidationError('Account is not active yet')
         self.context['user'] = user
         return data
 
