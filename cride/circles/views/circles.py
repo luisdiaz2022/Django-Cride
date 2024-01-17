64c86d404cacc6a50531b646f2f6ee946c68bfa8
Luis <luisedgucv@gmail.com>
2024-01-17 10:15:31 -0500

    added authentication token to the view and set up pagination

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -2,6 +2,7 @@
 
 # Django REST Framework
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
@@ -14,3 +15,11 @@ class CircleViewSet(viewsets.ModelViewSet):
 
     queryset = Circle.objects.all()
     serializer_class = CircleModelSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Restric list to public-only."""
+        queryset = Circle.objects.all()
+        if self.action == 'list':
+            return queryset.filter(is_public=True)
+        return queryset
