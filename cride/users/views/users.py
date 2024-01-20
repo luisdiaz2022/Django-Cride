cc1033ee79a0c53e99e10330b33204ec74f52bbe
Luis <luisedgucv@gmail.com>
2024-01-20 10:04:53 -0500

    migrated all the API VIEWs to a single view set function

--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -2,6 +2,7 @@
 
 # Django REST Framework
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import viewsets
@@ -19,14 +20,10 @@ class UserViewSet(viewsets.GenericViewSet):
     
     Handle sign up, login and account verification
     """
-    
 
-
-class UserLoginAPIView(APIView):
-    """User login API view."""
-
-    def post(self, request, *args, **kwargs):
-        """Handle HTTP POST request."""
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        """User sign in."""
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user, token = serializer.save()
@@ -35,25 +32,19 @@ class UserLoginAPIView(APIView):
             'access_token': token,
         }
         return Response(data, status=status.HTTP_201_CREATED)
-
-
-class UserSignUpAPIView(APIView):
-    """User sign up API view."""
-
-    def post(self, request, *args, **kwargs):
-        """Handle HTTP POST request."""
+    
+    @action(detail=False, methods=['post'])
+    def signup(self, request):
+        """User sign up."""
         serializer = UserSignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         data = UserModelSerializer(user).data
-
         return Response(data, status=status.HTTP_201_CREATED)
     
-class AccountVerificationAPIView(APIView):
-    """Account verification API view."""
-
-    def post(self, request, *args, **kwargs):
-        """Handle HTTP POST request."""
+    @action(detail=False, methods=['post'])
+    def verify(self, request):
+        """Account verification."""
         serializer = AccountVerificationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
