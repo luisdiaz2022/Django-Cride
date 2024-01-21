769884329ada7f4e69e7ef85bfe2001326825166
Luis <luisedgucv@gmail.com>
2024-01-21 11:51:43 -0500

    we generate a list of codes

--- cride/circles/managers/invitations.py
+++ cride/circles/managers/invitations.py
@@ -13,18 +13,16 @@ class InvitationManager(models.Manager):
     Used to handle code creation.
     """
 
+    CODE_LENGTH = 10
+
     def create(self, **kwargs):
         """Handle code creation.
         
         Used to handle code creation.
         """
-
-        CODE_LENGTH = 10
-
         pool = ascii_uppercase + digits + '.-'
         code = kwargs.get('code', ''.join(random.choices(pool, k=self.CODE_LENGTH)))
         while self.filter(code=code).exists():
             code = ''.join(random.choices(pool, k=self.CODE_LENGTH))
         kwargs['code'] = code
         return super(InvitationManager, self).create(**kwargs)
-        
